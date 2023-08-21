2117c41c00389c8d3681200cb3453cc9d61a7f7c
Yaroslav <yaroslavshyminair@gmail.com>
2023-08-21 20:08:17 +0300

    crud

---
+++ dao.py
@@ -0,0 +1,75 @@
+import asyncio
+
+from sqlalchemy import insert, select, update, delete
+
+from database import async_session_maker
+from models import User, Order
+
+
+async def create_user(
+        name: str,
+        login: str,
+        password: str,
+        notes: str = '',
+        is_conflict: bool = False,
+):
+    async with async_session_maker() as session:
+        query = insert(User).values(
+            name=name,
+            login=login,
+            password=password,
+            notes=notes,
+            is_conflict=is_conflict,
+            )
+        print(query)
+        await session.execute(query)
+        await session.commit()
+
+
+async def fetch_users(skip: int = 0, limit: int = 10):
+    async with async_session_maker() as session:
+        query = select(User).offset(skip).limit(limit)
+        result = await session.execute(query)
+        print(result.scalars().all()[0].__dict__)
+        return result
+
+
+async def get_user_by_id(user_id: int):
+    async with async_session_maker() as session:
+        query = select(User).filter_by(id=user_id)
+        print(query)
+        result = await session.execute(query)
+        # print(result.first())
+        print(result.scalar_one_or_none())
+
+
+async def update_user(user_id: int):
+    async with async_session_maker() as session:
+        query = update(User).where(User.id == user_id).values(name='adsf')
+        print(query)
+        result = await session.execute(query)
+        await session.commit()
+
+
+async def delete_user(user_id: int):
+    async with async_session_maker() as session:
+        query = delete(User).where(User.id == user_id)
+        print(query)
+        await session.execute(query)
+        await session.commit()
+
+
+async def main():
+    # await asyncio.gather(
+    #     create_user(
+    #         name='name1',
+    #         login='login2',
+    #         password='password1',
+    #         notes='*'*200
+    #     )
+    # )
+    # await asyncio.gather(fetch_users())
+    # await asyncio.gather(get_user_by_id(3))
+    # await asyncio.gather(update_user(1))
+    await asyncio.gather(delete_user(1))
+asyncio.run(main())
