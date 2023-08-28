84cf3c194f38e94d57e40e47c20455a78d3cf401
Yaroslav <yaroslavshyminair@gmail.com>
2023-08-28 16:39:29 +0100

    web

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import sentry_sdk
+from app.web_pages import route_web_pages
+
+sentry_sdk.init(
+    dsn="https://5b3180bbfd6ee4e660a01a4211eb1dac@o4505766519701504.ingest.sentry.io/4505766534905856",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
+
+
+app = FastAPI(
+    title='First our app',
+    description='we are not champions',
+    version='0.0.1',
+    debug=True,
+)
+app.mount('/app/static', StaticFiles(directory='app/static'), name='static')
+
+
+app.include_router(route_web_pages.router)
+
+@app.get('/')
+@app.post('/')
+async def main_page() -> dict:
+    return {'greeting': 'HELLO'}
+
+
+@app.get('/{user_name}')
+@app.get('/{user_name}/{user_nick}')
+async def user_page(user_name: str, user_nick: str = '', limit: int = 10, skip: int = 0) -> dict:
+    data = [i for i in range(1000)][skip:][:limit]
+
+
+    return {'user_name': user_name, 'user_nick': user_nick, 'data': data}
+
